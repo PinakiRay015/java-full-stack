bd060e53fc406ba08e683748c5904b82022b06e6
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-16 21:01:45 +0530

    data types and variables

--- basics_of_Java/dataTypes_and_variables.java
+++ basics_of_Java/dataTypes_and_variables.java
@@ -1,2 +1,20 @@
-package basics_of_Java;public class dataTypes_and_variables {
+package basics_of_Java;
+
+import java.util.Date;
+
+public class dataTypes_and_variables {
+    public static void main(String[] args) {
+        //primitive data types
+        byte a = 1;
+        short b = 12;
+        int c = 352;
+        float d = 34.5f;
+        double e = 12.4;
+        char f = 'a';
+        boolean g = true;
+
+        //non primitive data type
+        String name = "Rohan";
+        Date date = new Date();
+    }
 }
