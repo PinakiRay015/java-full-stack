c360a020272264358cac1600617585c965e9a3ff
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-17 11:08:27 +0530

    one dimensional array

---
+++ Fundamentals_of_Java/Arrays_And_Strings/Arrays.java
@@ -0,0 +1,15 @@
+package Fundamentals_of_Java.Arrays_And_Strings;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int []array = new int[5]; //initializing an array of size 5
+        for(int i=0,j=10 ; i<array.length ; i++,j++) //initializing values inside arrays
+        {
+            array[i] = j;
+        }
+        for(int i : array) //displaying of element inside array
+        {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
