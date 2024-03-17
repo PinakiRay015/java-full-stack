f10d54c490b6faae27c3762350c57b249a2ef49f
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-17 12:10:06 +0530

    arrays

--- Fundamentals_of_Java/Arrays_And_Strings/Arrays.java
+++ Fundamentals_of_Java/Arrays_And_Strings/Arrays.java
@@ -2,11 +2,12 @@ package Fundamentals_of_Java.Arrays_And_Strings;
 
 public class Arrays {
     public static void main(String[] args) {
-        int []array = new int[5]; //initializing an array of size 5
+        int []array = new int[5]; //initializing a one dimensional array of size 5
         for(int i=0,j=10 ; i<array.length ; i++,j++) //initializing values inside arrays
         {
             array[i] = j;
         }
+
         for(int i : array) //displaying of element inside array
         {
             System.out.print(array[i]+" ");
