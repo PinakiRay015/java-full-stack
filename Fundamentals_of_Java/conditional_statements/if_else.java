acc57be68d624fb75481c942374b0420faf63862
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-19 11:42:24 +0530

    if else statement in java

---
+++ Fundamentals_of_Java/conditional_statements/if_else.java
@@ -0,0 +1,16 @@
+package Fundamentals_of_Java.conditional_statements;
+
+public class if_else {
+    public static void main(String[] args)
+    {
+        int age = 12;
+        if(age>=18)
+        {
+            System.out.println("Eligible for voting");
+        }
+        else
+        {
+            System.out.println("Not eligible for voting");
+        }
+    }
+}
