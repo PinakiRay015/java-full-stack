144fdf8d7f33f3cdbfbacc7a14d46903530155b7
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-20 12:40:25 +0530

    fizzbuzz problem

--- Fundamentals_of_Java/conditional_statements/FizzBuzz_Problem_1.java
+++ Fundamentals_of_Java/conditional_statements/FizzBuzz_Problem_1.java
@@ -15,29 +15,14 @@ public class FizzBuzz_Problem_1 {
         int value = sc.nextInt();
         for(int i=1 ; i<=value ; i++)
         {
-            if(i % 3 == 0)
+            if(i%3==0 && i%5==0)
             {
-                if(i % 5 == 0)
-                {
-                    System.out.print("FizzBuzz ");
-                }
-                else
-                {
-                    System.out.print("Fizz ");
-                }
-            }
-            else if(i % 5 == 0)
-            {
-                if(i % 3 == 0)
-                {
-                    System.out.print("FizzBuzz ");
-                }
-                else
-                {
-                    System.out.print("Buzz ");
-                }
-            }
-            else
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            }else
             {
                 System.out.print(i+" ");
             }
