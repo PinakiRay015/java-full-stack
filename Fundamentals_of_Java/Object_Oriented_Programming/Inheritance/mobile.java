cb892065dc86e8691ee36a1ab00cb30b1d578433
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-23 17:16:51 +0530

    single level inheritance

--- Fundamentals_of_Java/Object_Oriented_Programming/Inheritance/mobile.java
+++ Fundamentals_of_Java/Object_Oriented_Programming/Inheritance/mobile.java
@@ -7,9 +7,18 @@ class android
         System.out.println("Runs on android os");
     }
 }
+
+class Samsung extends android
+{
+    void battery()
+    {
+        System.out.println("Gives 6000 Mah battery support");
+    }
+}
 public class mobile {
     public static void main(String[] args) {
-        android android = new android();
-        android.os();
+        Samsung samsung = new Samsung();
+        samsung.os();
+        samsung.battery();
     }
 }
