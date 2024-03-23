d1ca0db66fac9ae543f0b5d3b762513f7222c60b
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-23 17:01:16 +0530

    inheritance

---
+++ Fundamentals_of_Java/Object_Oriented_Programming/Inheritance/mobile.java
@@ -0,0 +1,15 @@
+package Fundamentals_of_Java.Object_Oriented_Programming.Inheritance;
+
+class android
+{
+    void os()
+    {
+        System.out.println("Runs on android os");
+    }
+}
+public class mobile {
+    public static void main(String[] args) {
+        android android = new android();
+        android.os();
+    }
+}
