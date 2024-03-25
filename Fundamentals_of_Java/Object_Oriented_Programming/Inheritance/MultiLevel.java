b8bef20b1284d6bb9d6a5d5985392e4315ed7283
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-25 09:04:11 +0530

    multilevel inheritance

--- Fundamentals_of_Java/Object_Oriented_Programming/Inheritance/MultiLevel.java
+++ Fundamentals_of_Java/Object_Oriented_Programming/Inheritance/MultiLevel.java
@@ -1,12 +1,34 @@
 package Fundamentals_of_Java.Object_Oriented_Programming.Inheritance;
 
-class Android
+class class1
 {
+    public void class1Property()
+    {
+        System.out.println("This is the property of class 1");
+    }
+}
 
+class class2 extends class1
+{
+    public void class2Property()
+    {
+        System.out.println("This is the property of class 2");
+    }
+}
+
+class class3 extends class2
+{
+    public void class3Property()
+    {
+        System.out.println("This is the property of class 3");
+    }
 }
 public class MultiLevel {
     public static void main(String[] args)
     {
-
+        class3 cls = new class3();
+        cls.class1Property();
+        cls.class2Property();
+        cls.class3Property();
     }
 }
