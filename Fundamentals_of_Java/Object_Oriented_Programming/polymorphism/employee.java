7c92a72518d4dcea233be34b0431c1991ae3c71f
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-03-26 18:43:19 +0530

    polymorpshim

--- Fundamentals_of_Java/Object_Oriented_Programming/polymorphism/employee.java
+++ Fundamentals_of_Java/Object_Oriented_Programming/polymorphism/employee.java
@@ -1,3 +1,5 @@
+//polymorphism in java. Polymorphism can be achieved by two methods. one is method overloading (compile time polymorphism) and other one is
+//method overriding (runtime polymorphism).
 package Fundamentals_of_Java.Object_Oriented_Programming.polymorphism;
 
 class experiencedEmployee
@@ -6,11 +8,16 @@ class experiencedEmployee
     {
         System.out.println("Experienced employee having Day shift");
     }
+
+    public void shift(int num1, int num2)  //method overloading. It is also called static polymorphism which is a compile time polymorphism.
+    {
+        System.out.println("Number of night shift employees are of "+num1+" and number of day shift employees are of "+num2);
+    }
 }
 
 class fresherEmployee extends experiencedEmployee
 {
-    @Override
+    @Override       //method overriding. It is also called dynamic polymorphism which is a runtime polymorphism.
     public void shift()
     {
         super.shift();
@@ -23,5 +30,6 @@ public class employee {
     {
         fresherEmployee freshEmp = new fresherEmployee();
         freshEmp.shift();
+        freshEmp.shift(12000 , 4000);
     }
 }
